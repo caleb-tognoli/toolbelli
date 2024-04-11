5d4492ab782d394043cd243b73f7c6fdb84b4803
Caleb Tognoli <tognolicaleb@gmail.com>
2024-04-11 16:21:38 +0200

    Added random date tool

--- toolbelli.js
+++ toolbelli.js
@@ -1,9 +1,13 @@
-function generate_random_numbers(amount, min, max) {
+function randi_range(min, max) { 
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
+}
+
+function generate_random_numbers(amount, min, max) {
     var numbers = []
     for(i = amount; i > 0; i--) {
-        numbers.push(Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled))
+        numbers.push(randi_range(min, max))
     }
     return numbers
 }
@@ -19,8 +23,10 @@ function array_to_string(array, separator) {
     return out
 }
 
-function get_string_UTC_date() {
-    var date = new Date()
+
+function get_string_UTC_date(date) {
+    if(date == null)
+        date = new Date()
     var date_string = ''
     date_string += date.getUTCFullYear() 
     date_string += '-' + (date.getUTCMonth()+1).toString().padStart(2, '0') 
@@ -28,8 +34,9 @@ function get_string_UTC_date() {
     return date_string
 }
 
-function get_string_UTC_time() {
-    var date = new Date()
+function get_string_UTC_time(date) {
+    if(date == null)
+        date = new Date()
     var time_string = ''
     time_string += date.getUTCHours().toString().padStart(2, '0') 
     time_string += ':' + date.getUTCMinutes().toString().padStart(2, '0')
@@ -37,6 +44,14 @@ function get_string_UTC_time() {
     return time_string
 }
 
-function get_string_UTC_timestamp() {
-    return get_string_UTC_date() + " " + get_string_UTC_time()
+function get_string_UTC_timestamp(date) {
+    if(date == null)
+        date = new Date()
+    return get_string_UTC_date(date) + " " + get_string_UTC_time(date)
+}
+
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
 }
